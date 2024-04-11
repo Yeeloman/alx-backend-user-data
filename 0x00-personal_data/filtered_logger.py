faedad3c6c322b246738a5f108d14e315825b102
Yeeloman <yami.onlyme@gmail.com>
2024-04-11 15:18:22 +0000

    tryin to fix task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-"""0x00. Personal data more text to test"""
+"""0x00. Personal data
+How to implement a log filter that will obfuscate PII fields
+How to encrypt a password and check the validity of an input password
+How to authenticate to a database using environment variables
+"""
 
 import logging
 import re
