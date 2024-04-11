ec829200e925d7a9128e5f18fea6d3ee757d66bc
Yeeloman <yami.onlyme@gmail.com>
2024-04-11 15:32:25 +0000

    last two tasks

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Encrypting passwords
+Check valid password"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """returns a salted, hashed password,
+    which is a byte string."""
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks if a pswrd is valid"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
